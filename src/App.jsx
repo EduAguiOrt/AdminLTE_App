13cbadd5fa8ab0cdd00884d9bd60823b4871801d
Eduardo A.O <utp0002328@alumno.utpuebla.edu.mx>
2023-06-04 20:16:57 -0600

    Componente de login agregado

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import Header from "./components/header.jsx";
 import Aside from "./components/aside.jsx";
 import Content from "./components/content.jsx";
 import Footer from "./components/footer.jsx";
-
+import Login from "./components/Login.jsx";
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Aside />
-      <Content />
-      <Footer />
+      <Login />
     </>
   );
 };
